17b2d1fb483344f2034548cdbd3349c40779e94e
Tom Rozendaal <tomrozendaal@macbookpro-2d19.finalist.nl>
2012-10-08 16:53:59 +0200

    prev, next implemented

--- src/Main.java
+++ src/Main.java
@@ -23,8 +23,10 @@ public class Main implements Runnable {
     private JFrame frame = new JFrame();
     private JPanel panel = new JPanel(new BorderLayout());
     private JLabel lblStatus = new JLabel();
-
-    private JButton btnGo = new JButton("Go");
+    
+    private JButton reload = new JButton(new ImageIcon("src/img/refresh.png"));
+    private JButton prev = new JButton(new ImageIcon("src/img/prev.png"));
+    private JButton next = new JButton(new ImageIcon("src/img/next.png"));
     private JTextField txtURL = new JTextField();
     private JProgressBar progressBar = new JProgressBar();
 
@@ -36,19 +38,55 @@ public class Main implements Runnable {
         ActionListener al = new ActionListener() {
             @Override public void actionPerformed(ActionEvent e) {
                 loadURL(txtURL.getText());
+                txtURL.setScrollOffset(0);
             }
         };
+        
+        ActionListener alReload = new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		reload();
+        	}
+        };
+        
+        ActionListener alPrev = new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		goBack();
+        	}
+        };
+        
+        ActionListener alNext = new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		goNext();
+        	}
+        };
+        
+        txtURL.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                SwingUtilities.invokeLater( new Runnable() {
+                	public void run() {
+                    	txtURL.selectAll();              
+                    }
+                });
+            }
+        });
 
-        btnGo.addActionListener(al);
         txtURL.addActionListener(al);
+        reload.addActionListener(alReload);
+        prev.addActionListener(alPrev);
+        next.addActionListener(alNext);
 
         progressBar.setPreferredSize(new Dimension(150, 18));
         progressBar.setStringPainted(true);
+        
+        JPanel navigation = new JPanel();
+        navigation.add(prev, BorderLayout.WEST);
+        navigation.add(next, BorderLayout.CENTER);
+        navigation.add(reload, BorderLayout.EAST);
 
         JPanel topBar = new JPanel(new BorderLayout(5, 0));
         topBar.setBorder(BorderFactory.createEmptyBorder(3, 5, 3, 5));
+        topBar.add(navigation, BorderLayout.WEST);
         topBar.add(txtURL, BorderLayout.CENTER);
-        topBar.add(btnGo, BorderLayout.EAST);
 
 
         JPanel statusBar = new JPanel(new BorderLayout(5, 0));
@@ -59,8 +97,17 @@ public class Main implements Runnable {
         panel.add(topBar, BorderLayout.NORTH);
         panel.add(jfxPanel, BorderLayout.CENTER);
         panel.add(statusBar, BorderLayout.SOUTH);
-
+        
         frame.getContentPane().add(panel);
+    }
+    
+    protected JComponent makeTextPanel(String text) {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
     }
 
     private void createScene() {
@@ -77,6 +124,7 @@ public class Main implements Runnable {
                         SwingUtilities.invokeLater(new Runnable() {
                             @Override public void run() {
                                 frame.setTitle(newValue);
+                                txtURL.setScrollOffset(0);
                             }
                         });
                     }
@@ -117,18 +165,17 @@ public class Main implements Runnable {
                 engine.getLoadWorker()
                         .exceptionProperty()
                         .addListener(new ChangeListener<Throwable>() {
-
                             public void changed(ObservableValue<? extends Throwable> o, Throwable old, final Throwable value) {
                                 if (engine.getLoadWorker().getState() == FAILED) {
                                     SwingUtilities.invokeLater(new Runnable() {
                                         @Override public void run() {
                                             JOptionPane.showMessageDialog(
-                                                    panel,
-                                                    (value != null) ?
-                                                    engine.getLocation() + "\n" + value.getMessage() :
-                                                    engine.getLocation() + "\nUnexpected error.",
-                                                    "Loading error...",
-                                                    JOptionPane.ERROR_MESSAGE);
+                                                panel,
+                                                (value != null) ?
+                                                engine.getLocation() + "\n" + value.getMessage() :
+                                                engine.getLocation() + "\nUnexpected error.",
+                                                "Loading error...",
+                                                JOptionPane.ERROR_MESSAGE);
                                         }
                                     });
                                 }
@@ -153,6 +200,30 @@ public class Main implements Runnable {
             }
         });
     }
+    
+    public void reload(){
+    	Platform.runLater(new Runnable() {
+    		public void run(){
+    			engine.reload();
+    		}
+    	});
+    }
+    
+    public void goBack(){
+    	Platform.runLater(new Runnable(){
+    		public void run(){
+    			engine.executeScript("history.back()");
+    		}
+    	});
+    }
+    
+    public void goNext(){
+    	Platform.runLater(new Runnable(){
+    		public void run(){
+    			engine.executeScript("history.go(+1)");
+    		}
+    	});
+    }
 
     private static String toURL(String str) {
         try {
@@ -163,7 +234,6 @@ public class Main implements Runnable {
     }
 
     @Override public void run() {
-
         frame.setPreferredSize(new Dimension(1024, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
